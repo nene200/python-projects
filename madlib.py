78ee1391e17127b4be50fd9a5a8cf3576e89c06d
nene200 <okwaranneoma2004@gmail.com>
2022-09-09 13:41:13 -0700

    first commit

---
+++ madlib.py
@@ -0,0 +1,9 @@
+"""Madlibs Generation
+
+
+"""
+# loop back to this point once code finishes
+
+loop = 1
+
+while(loop < 10)
---
+++ quiz.py
@@ -0,0 +1,16 @@
+print("welcome to my quiz! " )
+
+playing = input("Do you want to play? ")
+print(playing)
+if playing.lower == "yes":
+    quit()
+
+print("okay! lets play:)")
+
+answer = input('what does cpu stand for? ')
+if answer == "central processing unit":
+    print('correct!')
+else:
+    print('Incorrect!')
+
+
